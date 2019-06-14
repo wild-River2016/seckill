15104ed4cc381f8a0ac7f477547ef830f6f2dac9
lsh1990 <xtlsh1990@126.com>
2019-06-14 16:46:46 +0800

    modify 利用SpringMvc自定义参数解析器

--- src/main/java/com/lsh/config/UserArgumentResolver.java
+++ src/main/java/com/lsh/config/UserArgumentResolver.java
@@ -17,7 +17,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @ClassName UserArgumentResolver
- * @Description: TODO
+ * @Description: user对象参数解析器
  * @Author lsh
  * @Date 2019/6/12 21:42
  * @Version
@@ -28,18 +28,34 @@ public class UserArgumentResolver implements HandlerMethodArgumentResolver {
     @Autowired
     MiaoshaUserService userService;
 
+    /**
+     * 判断是否启用分解功能
+     * @param methodParameter
+     * @return
+     */
     @Override
     public boolean supportsParameter(MethodParameter methodParameter) {
         Class<?> clazz = methodParameter.getParameterType();
         return clazz == MiaoshaUser.class;
     }
 
+    /**
+     * 解析参数
+     * @param methodParameter
+     * @param modelAndViewContainer
+     * @param nativeWebRequest
+     * @param webDataBinderFactory
+     * @return
+     * @throws Exception
+     */
     @Override
     public Object resolveArgument(MethodParameter methodParameter, ModelAndViewContainer modelAndViewContainer,
                                   NativeWebRequest nativeWebRequest, WebDataBinderFactory webDataBinderFactory) throws Exception {
         HttpServletRequest request = nativeWebRequest.getNativeRequest(HttpServletRequest.class);
         HttpServletResponse response = nativeWebRequest.getNativeResponse(HttpServletResponse.class);
+        //获取token参数
         String paramToken = request.getParameter(MiaoshaUserService.COOKI_NAME_TOKEN);
+        //获取cookie中的token
         String cookieToken = getCookieValue(request, MiaoshaUserService.COOKI_NAME_TOKEN);
         if(StringUtils.isEmpty(cookieToken) && StringUtils.isEmpty(paramToken)) {
             return null;
--- src/main/java/com/lsh/config/WebConfig.java
+++ src/main/java/com/lsh/config/WebConfig.java
@@ -3,22 +3,27 @@ package com.lsh.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import java.util.List;
 
 /**
  * @ClassName WebConfig
- * @Description: TODO
+ * @Description: SpringBoot自定义参数解析器
  * @Author lsh
  * @Date 2019/6/12 21:39
  * @Version
  */
 @Configuration
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig extends WebMvcConfigurationSupport {
     @Autowired
     UserArgumentResolver userArgumentResolver;
 
+    /**
+     *添加参数解析器
+     * @param argumentResolvers
+     */
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         argumentResolvers.add(userArgumentResolver);
