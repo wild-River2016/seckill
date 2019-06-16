8b552e45c0e384bd439f6a9dd97ef8c9e4ab180a
lsh <lsh>
2019-06-16 18:17:35 +0800

    add 秒杀页面静态化

--- src/main/java/com/lsh/redis/RedisService.java
+++ src/main/java/com/lsh/redis/RedisService.java
@@ -44,7 +44,21 @@ public class RedisService {
             returnToPool(jedis);
         }
     }
-
+    /**
+     * 删除
+     * */
+    public boolean delete(KeyPrefix prefix, String key) {
+        Jedis jedis = null;
+        try {
+            jedis =  jedisPool.getResource();
+            //生成真正的key
+            String realKey  = prefix.getPrefix() + key;
+            long ret =  jedis.del(key);
+            return ret > 0;
+        }finally {
+            returnToPool(jedis);
+        }
+    }
     /**
      * 获取当个对象
      * */
