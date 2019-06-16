34f3e9d10b9377abe2ed240334893e4d7edb6951
lsh <lsh>
2019-06-16 10:47:30 +0800

    modify 修改配置信息&&增加war打包方式

--- src/main/java/com/lsh/MainApplication.java
+++ src/main/java/com/lsh/MainApplication.java
@@ -2,11 +2,19 @@ package com.lsh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(MainApplication.class, args);
     }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MainApplication.class);
+    }
+
 }
