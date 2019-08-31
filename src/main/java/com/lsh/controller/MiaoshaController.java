d944a0f2e1e7376d5993a308b56c4a863ae4668e
lsh <lsh>
2019-08-31 17:57:07 +0800

    modify brach合并冲突

--- src/main/java/com/lsh/controller/MiaoshaController.java
+++ src/main/java/com/lsh/controller/MiaoshaController.java
@@ -69,7 +69,7 @@ public class MiaoshaController implements InitializingBean {
 		if(order != null) {
 			return Result.error(CodeMsg.REPEATE_MIAOSHA);
 		}
-		//入队 测试
+		//入队 测试 brach测试
 		MiaoshaMessage mm = new MiaoshaMessage();
 		mm.setUser(user);
 		mm.setGoodsId(goodsId);
